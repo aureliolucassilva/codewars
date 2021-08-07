12648b2099cbb969acaafdf9e7a21d0f15e4acb1
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2021-08-07 14:57:30 -0300

    Create 6kyu - F.scala

---
+++ scala/6kyu - F.scala
@@ -0,0 +1,26 @@
+import scala.math._
+
+object VolTank {
+
+  def tankVol(h: Int, d: Int, vt: Int): Int = {
+    val r: Double = d/2.0
+    val l: Double = 4.0 * vt/(Pi*pow(d,2))
+    var m: Double = 0.0
+    var angle: Double = 0.0
+    var volume: Double = 0.0
+
+    if(h < r){
+      m = r - h
+      angle = acos(m/r) * 2.0
+      volume = 0.5 * pow(r,2) * (angle - sin(angle)) * l
+    } else if(h == r){
+      volume = vt/2
+    } else if(h > r) {
+      m = h - r
+      angle = acos(m/r) * 2.0
+      volume = vt - 0.5 * pow(r,2) * (angle - sin(angle)) * l
+    }
+
+    volume.toInt
+  }
+}
